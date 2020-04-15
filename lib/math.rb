17c51c2c497101acf2a12a55239cabdc59edf7a2
Daniel Cooper <cooperdaniel5141@gmail.com>
2020-04-15 21:34:33 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,28 @@
 def addition(num1, num2)
-
+  sum_one = num1 + num2
+  return sum_one
 end
 
 def subtraction(num1, num2)
-
+  sum_two = num1 - num2
+  return sum_two
 end
 
 def division(num1, num2)
-
+  sum_three = num1 / num2
+  return sum_three
 end
 
 def multiplication(num1, num2)
-
+  sum_four = num1 * num2
+  return sum_four
 end
 
 def modulo(num1, num2)
-
+  sum_five = num1 % num2
+  return sum_five
 end
 
 def square_root(num)
-
+  return Math.sqrt(num)
 end
